3073ad1c53ac994f1a974b59ce7a943dd880ba07
Amanda Nagai <amanda.nagai@gmail.com>
2017-06-11 17:32:19 -0700

    delete and checkbox working

--- src/ToDo.js
+++ src/ToDo.js
@@ -5,7 +5,8 @@ class ToDo extends Component {
   render() {
     return (
       <div id='todo'>
-        <p>{this.props.text}</p>
+        <input type="checkbox" onClick={this.props.handleChecked}/>
+        <p>&nbsp;{this.props.text}&nbsp;</p>
         <button onClick={this.props.handleDelete}>X</button>
       </div>
     )
--- src/TodoList.js
+++ src/TodoList.js
@@ -8,11 +8,10 @@ class TodoList extends Component {
     super(props);
     this.state = {
       doList: [],
-      tempDo: ''
+      tempDo: '',
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleChange(e) {
@@ -32,32 +31,74 @@ class TodoList extends Component {
   handleSubmit(e) {
     e.preventDefault();
     let doList = this.state.doList.slice();
-    doList.push({key: this.guid(), todo: this.state.tempDo});
+    doList.push({key: this.guid(), todo: this.state.tempDo, isChecked: false});
     this.setState({
       tempDo: '',
       doList: doList
     })
   };
 
-  handleDelete(e) {
-    debugger;
-    e.preventDefault();
+  handleDelete(id) {
     let doList = this.state.doList.slice();
     for (var i = 0; i < doList.length; i++) {
-      if (doList[i].key === e.target.key) {
+      if (doList[i].key === id) {
         doList.splice(i, 1);
       }
     }
     this.setState({doList});
   };
 
+  handleChecked(id) {
+    let doList = this.state.doList.slice();
+    for (var i = 0; i < doList.length; i++) {
+      if (this.state.doList[i].key === id) {
+        doList[i].isChecked = doList[i].isChecked? false : true;
+      }
+    }
+    this.setState(
+      {doList}
+    )
+  }
+
+  handleEdit(id) {
+    let doList = this.state.doList.slice();
+    for (var i = 0; i < doList.length; i++) {
+      if (this.state.doList[i].key === id) {
+        doList[i].isChecked = doList[i].isChecked? false : true;
+      }
+    }
+    this.setState(
+      {doList}
+    )
+  }
+
   render() {
     let list = this.state.doList.map((td) => {
-      return <ToDo key={td.key} text={td.todo} handleDelete={this.handleDelete}/>
+      if (!td.isChecked) {
+        return <ToDo key={td.key}                           
+                text={td.todo}
+                handleDelete={this.handleDelete.bind(this, td.key)}
+                handleChecked={this.handleChecked.bind(this, td.key)}
+                checked
+                />
+      }  else {
+        return <ToDo key={td.key}                           
+                text={td.todo}
+                handleDelete={this.handleDelete.bind(this, td.key)}
+                handleChecked={this.handleChecked.bind(this, td.key)}
+                />
+      }
+      // else {
+      //   return <Alert/>
+      // }
     })
+
     return (
       <div>
-        <TodoForm handleSubmit={this.handleSubmit} handleChange={this.handleChange} tempDo={this.state.tempDo}/>
+        <TodoForm handleSubmit={this.handleSubmit}
+        handleChange={this.handleChange}
+        tempDo={this.state.tempDo}
+        />
         <ul>
           {list}
         </ul>
